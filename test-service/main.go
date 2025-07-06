39b6296553c0aa8f1d0a4bef1dcb2c785ba088df
monotako <monotako@proton.me>
2025-07-06 22:35:00 +0100

    AGG-2: Test if init-serv ci is triggered 2

--- test-service/main.go
+++ test-service/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("I want these changes to not trigger initial-service CI!")
+	fmt.Println("I want these changes to not trigger initial-service CI!!")
 }
